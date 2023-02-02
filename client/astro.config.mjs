d5b93aaa4dd6590b7a210b38186bbc22c748a0a5
Mandar Patil <mandarons@pm.me>
2023-02-02 05:15:30 +0000

    wapar client refactoring

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,17 +1,19 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+// import node from '@astrojs/node';
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import react from "@astrojs/react";
+// import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  })
+  integrations: [tailwind(),
+    // react()
+  ],
+  // output: 'server',
+  // adapter: node({
+  //   mode: 'standalone'
+  // })
 });
