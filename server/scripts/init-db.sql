dd3fe7099efc39df0e15717320dc95919c1b2e07
Mandar Patil <mandarons@pm.me>
2023-02-05 05:26:31 +0000

    server cleanup

--- server/scripts/init-db.sql
+++
@@ -1,8 +0,0 @@
-CREATE USER waparuser WITH PASSWORD 'wapar-user';
-GRANT ALL ON SCHEMA public to waparuser;
-CREATE DATABASE wapardev;
--- TODO: set waparuser as owner of wapardev;
-GRANT ALL PRIVILEGES ON DATABASE wapardev TO waparuser;
--- TOOD: run below as waparuser on wapardev;
-CREATE EXTENSION pgcrypto;
-CREATE EXTENSION "uuid-ossp";
