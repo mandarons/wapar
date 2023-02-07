3b935e82dca234a458831d82a63757eb8091f584
Mandar Patil <mandarons@pm.me>
2023-02-07 17:17:02 +0000

    added model sync in bootstrap;

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,8 +2,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestFastifyApplication, FastifyAdapter } from '@nestjs/platform-fastify';
+import { Installation } from './installations/installation.model';
+import { Heartbeat } from './heartbeats/heartbeat.model';
 async function bootstrap() {
     const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+    Installation.sync();
+    Heartbeat.sync();
     await app.listen(3000, '0.0.0.0');
 }
 
