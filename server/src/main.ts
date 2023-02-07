43df06a7f2f59b4b755394057a4b37241cbb0512
Mandar Patil <mandarons@pm.me>
2023-02-07 14:07:14 +0000

    added fastify;

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,10 @@
 /* istanbul ignore file */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { NestFastifyApplication, FastifyAdapter } from '@nestjs/platform-fastify';
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+    const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+    await app.listen(3000, '0.0.0.0');
 }
 
 bootstrap();
