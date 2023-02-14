695a792bf33908d38c3025cf5bfe25f30e307a4b
Mandar Patil <mandarons@pm.me>
2023-02-14 17:06:47 +0000

    adde nextjs web client;

---
+++ web/src/pages/about.tsx
@@ -0,0 +1,13 @@
+import Footer from '@/components/footer';
+import Navbar from '@/components/navbar';
+
+const About = () => {
+  return (
+    <>
+      <Navbar selectedItem=""></Navbar>
+      <h1 className="text-center">About Wapar</h1>
+      <Footer dataGeneratedAt={null}></Footer>
+    </>
+  );
+};
+export default About;
