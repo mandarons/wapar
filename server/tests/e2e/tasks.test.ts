0c32a9b7c66a79f44ede900c088ed8dc05696331
Mandar Patil <mandarons@pm.me>
2023-02-16 17:08:55 +0000

    more tests;

--- server/tests/e2e/tasks.test.ts
+++ server/tests/e2e/tasks.test.ts
@@ -53,6 +53,24 @@ describe('Tasks', async () => {
         result.should.be.true;
         (await installationService.getMissingIPInfo()).length.should.be.equal(0);
     });
+    it('Should populate IP info in chunks of 100', async () => {
+        const mock = Sinon.stub(axios, 'post');
+        mock.callsFake((url, data) => {
+            return new Promise((resolve) =>
+                resolve({ data: (data as string[]).map((e: string) => ({ query: e, country: faker.address.country(), region: faker.address.state() })) }),
+            );
+        });
+        const installationRecords = new Array(110);
+        for (let i = 0; i < installationRecords.length; ++i) installationRecords[i] = dataUtils.createInstallationRecord();
+
+        await Installation.bulkCreate(installationRecords);
+        let result = await service.updateIpInfo();
+        result.should.be.true;
+        (await installationService.getMissingIPInfo()).length.should.be.equal(10);
+        result = await service.updateIpInfo();
+        result.should.be.true;
+        (await installationService.getMissingIPInfo()).length.should.be.equal(0);
+    });
     it('Should return false if nothing to do', async () => {
         const result = await service.updateIpInfo();
         result.should.be.false;
