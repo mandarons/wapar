64bd37ca9329902ba3bccbe7f86cf155d1e0b03b
Mandar Patil <mandarons@pm.me>
2023-02-16 17:25:13 +0000

    more tests;

--- server/src/heartbeats/heartbeats.module.ts
+++ server/src/heartbeats/heartbeats.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { InstallationsModule } from '../installations/installations.module';
 import { HeartbeatController } from './heartbeat.controller';
 import { Heartbeat } from './heartbeat.model';
 import { HeartbeatService } from './heartbeat.service';
 
 @Module({
-    imports: [SequelizeModule.forFeature([Heartbeat])],
+    imports: [SequelizeModule.forFeature([Heartbeat]), InstallationsModule],
     providers: [HeartbeatService],
     controllers: [HeartbeatController],
 })
