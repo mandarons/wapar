1136674a1d6885208477d323b78370b8009e79e4
Mandar Patil <mandarons@pm.me>
2023-02-17 16:56:39 +0000

    added map;

--- web/src/components/footer.tsx
+++ web/src/components/footer.tsx
@@ -1,27 +1,29 @@
 import { useEffect, useState } from 'react';
 
 const Footer = ({ dataGeneratedAt }: { dataGeneratedAt: string | null } = { dataGeneratedAt: null }) => {
-  const [data, setData] = useState(dataGeneratedAt);
-  useEffect(() => {
-    if (data) {
-      setData(new Date(data).toLocaleString());
-    }
-  }, [data]);
-  return <>
-      <div className="fixed bottom-0 w-full border-t">
-        <div className="flex items-center justify-between p-4">
-          <div className="flex items-center"></div>
-          <div className="flex items-end space-x-4">
-            {data && (
-              <p className="text-xs font-medium text-gray-600">
-                Generated at: <span data-testid="data-generated-at">{data}</span>
-              </p>
-            )}
-            <p className="text-xs font-medium text-gray-600">Copyright &copy; 2023 Mandar Patil</p>
-          </div>
-        </div>
-      </div>
-    </>;
+    const [data, setData] = useState(dataGeneratedAt);
+    useEffect(() => {
+        if (data) {
+            setData(new Date(data).toLocaleString());
+        }
+    }, [data]);
+    return (
+        <>
+            <div className="fixed bottom-0 w-full">
+                <div className="flex items-center justify-between p-4">
+                    <div className="flex items-center"></div>
+                    <div className="flex items-end space-x-4">
+                        {data && (
+                            <p className="text-xs font-medium text-gray-600">
+                                Generated at: <span data-testid="data-generated-at">{data}</span>
+                            </p>
+                        )}
+                        <p className="text-xs font-medium text-gray-600">Copyright &copy; 2023 Mandar Patil</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Footer;
