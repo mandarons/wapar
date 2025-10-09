0131d5826275d444d5e1b6dd2c48c3d01e0dcc77
Mandar Patil <mandarons@pm.me>
2025-10-09 00:37:09 +0000

    Refactor code structure for improved readability and maintainability

--- server/src/tasks/tasks.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TasksService } from './tasks.service';
-import { HttpModule } from '@nestjs/axios';
-import { InstallationsModule } from '../installations/installations.module';
-@Module({
-    providers: [TasksService],
-    imports: [HttpModule, InstallationsModule],
-})
-export class TasksModule {}
